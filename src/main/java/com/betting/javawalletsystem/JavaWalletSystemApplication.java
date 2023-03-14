4aa9a6f9b46339228e5257b8b99c79636e978e82
jbonn360 <justinbonnici94@gmail.com>
2023-03-14 18:52:35 +0100

    Initial commit

---
+++ src/main/java/com/betting/javawalletsystem/JavaWalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.betting.javawalletsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWalletSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betting/javawalletsystem/JavaWalletSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betting.javawalletsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWalletSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
